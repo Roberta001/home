8c93eb77d04ea20b752e18b1c70c2d663bc7026f
Roberta001 <112218924+Roberta001@users.noreply.github.com>
2024-05-01 13:29:16 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,8 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, ChartArea, BroadcastTower, Music, comment} from "@vicons/fa"; // 注意使用正确的类别
-import { robot } from "@vicons/tabler";
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, ChartArea, BroadcastTower, Music, Microsoft, Comments} from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -75,6 +74,8 @@ const siteIcon = {
   ChartArea,
   BroadcastTower,
   Music,
+  Microsoft,
+  Comments,
 };
 
 // 链接跳转
